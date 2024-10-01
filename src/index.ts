a544fec20383c002b34e9d15fb46d00681c2424a
Timothy Miamba <timal0361@gmail.com>
2024-10-01 17:20:48 +0200

    feat: Add mongoose configuration for MongoDB connection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+// Mongoose configuration
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_URI || "";
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
