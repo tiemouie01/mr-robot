9ff001d99d16effdc126a72f8ade2d0ae0f6cf6f
Timothy Miamba <timal0361@gmail.com>
2024-10-01 20:57:30 +0200

    chore: Include virtual relations in the schema declaration

--- src/models/category.ts
+++ src/models/category.ts
@@ -1,13 +1,20 @@
 import { Schema, model } from "mongoose";
 
-const CategorySchema = new Schema({
-  name: String,
-  description: String,
-});
-
-CategorySchema.virtual("url").get(function () {
-  return `/shop/category/${this._id}`;
-});
+const CategorySchema = new Schema(
+  {
+    name: String,
+    description: String,
+  },
+  {
+    virtuals: {
+      url: {
+        get() {
+          return `/shop/category/${this._id}`;
+        },
+      },
+    },
+  }
+);
 
 const Category = model("Category", CategorySchema);
 
--- src/models/item.ts
+++ src/models/item.ts
@@ -1,16 +1,23 @@
 import { Schema, model } from "mongoose";
 
-const ItemSchema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
-  price: { type: Number, required: true },
-  stock: Number,
-});
-
-ItemSchema.virtual("url").get(function () {
-  return `/shop/item/${this._id}`;
-});
+const ItemSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+    price: { type: Number, required: true },
+    stock: Number,
+  },
+  {
+    virtuals: {
+      url: {
+        get() {
+          return `/shop/item/${this._id}`;
+        },
+      },
+    },
+  }
+);
 
 const Item = model("Item", ItemSchema);
 
