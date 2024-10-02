92e1ce12986ee9ace5b3d540e0b485fa8dc8920d
Timothy Miamba <timal0361@gmail.com>
2024-10-02 14:12:47 +0200

    refactor: Add middleware to serve stylesheets

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,7 @@ app.set("view engine", "ejs");
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
